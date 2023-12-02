ca0cefc35f232575c49be9d28d71fc5612ccbb0d
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-02 19:11:31 +0300

    refactored router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import { studentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app = express();
 
 //parsers
@@ -11,8 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // application-routes
-app.use('/api/v1/students', studentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running');
