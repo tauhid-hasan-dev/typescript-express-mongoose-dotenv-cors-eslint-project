ab2fcce20ec3493a74d2dfffa7c19ae694809d28
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-06 21:03:58 +0300

    middleware udpated

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,31 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+// it will work before document processing
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new Error('This department is already exist');
+  }
+  next();
+});
+
+// and it will work before the query (this is query middle ware)- if I try to update a deleted item
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();  //{id: 'sdlkfjdslkfjldsfj'}
+
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error('This department does not exist! ');
+  }
+
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
