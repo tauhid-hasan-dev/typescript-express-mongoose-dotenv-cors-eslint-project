77394984d384e6e6f9d168a66880459bbebdfe82
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-09 11:45:01 +0300

    transaction and roll back is working fine

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { TStudent } from '../student/student.interface';
 import { Student } from '../student/student.model';
@@ -5,6 +6,8 @@ import { AcademicSemester } from './../academicSemester/academicSemester.model';
 import { TUser } from './user.interface';
 import { User } from './user.model';
 import { generateStudentId } from './user.utils';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
 
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   // create a user object
@@ -21,20 +24,35 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   );
 
-  //set  generated id
-  userData.id = await generateStudentId(admissionSemester!); // we assert here that, admissionSemester will never be a null value.
+  //set generated id to userData
+  userData.id = await generateStudentId(admissionSemester!); // we assert(!) here that, admissionSemester will never be a null value.
 
-  // create a user
-  const newUser = await User.create(userData);
+  //declare a session for transaction
 
-  //create a student
-  if (Object.keys(newUser).length) {
+  const session = await mongoose.startSession();
+  try {
+    await session.startTransaction();
+    // create a user (transaction - 1)
+    const newUser = await User.create([userData], { session }); // array (for transaction)
+
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to created user');
+    }
     // set id , _id as user
-    payload.id = newUser.id;
-    payload.user = newUser._id; //reference _id
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
 
-    const newStudent = await Student.create(payload);
+    // create a student ( transaction - 2)
+    const newStudent = await Student.create([payload], { session });
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
+    }
+    await session.commitTransaction();
+    await session.endSession();
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
 
