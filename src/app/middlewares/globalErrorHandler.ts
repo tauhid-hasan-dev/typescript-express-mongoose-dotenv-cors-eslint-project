4251758859886e575c7f918a09805767b15ceb8f
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-10 00:42:00 +0300

    handling zod error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,23 +2,66 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { ZodError, ZodIssue } from 'zod';
 
 //! global error handler has 4 parameter
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // setting default values
+  let statusCode = err.statuscode || httpStatus.INTERNAL_SERVER_ERROR;
+  let message = err.message || 'Something went wrong!';
 
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  type TGenericErrorResponse = {
+    statusCode: number;
+    message: string;
+    errorSource: TErrorSource;
+  };
+
+  const handleZodError = (err: ZodError): TGenericErrorResponse => {
+    const errorSource: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    const statusCode = 400;
+
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSource,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  }
+  //ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
+    err,
+    stack: err.stack,
+
+    //error: err,
   });
 };
 
