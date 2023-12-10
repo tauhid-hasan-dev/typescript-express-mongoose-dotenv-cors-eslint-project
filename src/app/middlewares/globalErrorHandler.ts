60e75107dbbfb486f5d9a339974c8ff5030105ad
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-10 15:20:47 +0300

    handle duplicate error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -10,6 +10,7 @@ import { TErrorSource } from '../interface/error';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidaitonError';
 import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 
 //! global error handler has 4 parameter
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
@@ -39,13 +40,18 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSource = simplifiedError?.errorSource;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
   }
   //ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
     errorSource,
-    /*  err,  */ // from this global error we can see the type of them actual error.
+    err, // from this global error we can see the type of them actual error.
     stack: config.NODE_ENV === 'development' ? err?.stack : null,
 
     //error: err,
