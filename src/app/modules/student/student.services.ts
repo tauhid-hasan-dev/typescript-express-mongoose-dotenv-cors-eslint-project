c7049560d44d8e39ac9b7c28d9bb2b14e5929a8f
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-12 23:13:58 +0300

    paginating, sorting, limit, filtering done

--- src/app/modules/student/student.services.ts
+++ src/app/modules/student/student.services.ts
@@ -7,33 +7,35 @@ import mongoose from 'mongoose';
 import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 import { studentSearchableFields } from './student.constant';
+import QueryBuilder from '../../builder/QueryBuilder';
 
 const getStudentFromDB = async (query: Record<string, unknown>) => {
-  const queryObj = { ...query }; // copying req.query object so that we can mutate the copy object
-
-  let searchTerm = ''; // SET DEFAULT VALUE
+  /*
+  const queryObj = { ...query }; // copying req.query object so that we can mutate the copy object 
+   
+  let searchTerm = '';   // SET DEFAULT VALUE 
 
   // IF searchTerm  IS GIVEN SET IT
   if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string;
+    searchTerm = query?.searchTerm as string; 
   }
 
-  // HOW OUR FORMAT SHOULD BE FOR PARTIAL MATCH  :
-  /*  { email: { $regex : query.searchTerm , $options: i}}
+ // HOW OUR FORMAT SHOULD BE FOR PARTIAL MATCH  : 
+  { email: { $regex : query.searchTerm , $options: i}}
   { presentAddress: { $regex : query.searchTerm , $options: i}}
-  { 'name.firstName': { $regex : query.searchTerm , $options: i}} */
+  { 'name.firstName': { $regex : query.searchTerm , $options: i}}
 
+  
   // WE ARE DYNAMICALLY DOING IT USING LOOP
-  const searchQuery = Student.find({
-    $or: studentSearchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
+   const searchQuery = Student.find({
+     $or: studentSearchableFields.map((field) => ({
+       [field]: { $regex: searchTerm, $options: 'i' },
     })),
-  });
+   });
 
-  // FILTERING fUNCTIONALITY:
-
+   // FILTERING fUNCTIONALITY:
   const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
-  excludeFields.forEach((el) => delete queryObj[el]); // DELETING THE FIELDS SO THAT IT CAN'T MATCH OR FILTER EXACTLY
+   excludeFields.forEach((el) => delete queryObj[el]);  // DELETING THE FIELDS SO THAT IT CAN'T MATCH OR FILTER EXACTLY
 
   const filterQuery = searchQuery
     .find(queryObj)
@@ -45,25 +47,78 @@ const getStudentFromDB = async (query: Record<string, unknown>) => {
       },
     });
 
+ 
   // SORTING FUNCTIONALITY:
-
-  let sort = '-createdAt'; // SET DEFAULT VALUE
-
-  // IF sort  IS GIVEN SET IT
-
-  if (query.sort) {
+  let sort = '-createdAt'; // SET DEFAULT VALUE 
+ 
+ // IF sort  IS GIVEN SET IT
+   if (query.sort) {
     sort = query.sort as string;
   }
 
-  const sortQuery = filterQuery.sort(sort);
-  let limit = 1;
+   const sortQuery = filterQuery.sort(sort);
+
+   // PAGINATION FUNCTIONALITY:
+   let page = 1; // SET DEFAULT VALUE FOR PAGE 
+   let limit = 1; // SET DEFAULT VALUE FOR LIMIT 
+   let skip = 0; // SET DEFAULT VALUE FOR SKIP
+
+
+  // IF limit IS GIVEN SET IT
   if (query.limit) {
     limit = Number(query.limit);
   }
 
-  const limitQuery = await sortQuery.limit(limit);
+  // IF page IS GIVEN SET IT
+  if (query.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
 
-  return limitQuery;
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = paginateQuery.limit(limit);
+
+  
+  // FIELDS LIMITING FUNCTIONALITY:
+
+  // HOW OUR FORMAT SHOULD BE FOR PARTIAL MATCH 
+
+  fields: 'name,email'; // WE ARE ACCEPTING FROM REQUEST
+  fields: 'name email'; // HOW IT SHOULD BE 
+
+  let fields = '-__v'; // SET DEFAULT VALUE
+
+  if (query.fields) {
+    fields = (query.fields as string).split(',').join(' ');
+
+  }
+
+  const fieldQuery = await limitQuery.select(fields);
+
+  return fieldQuery;
+
+  */
+
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await studentQuery.modelQuery;
+  return result;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
