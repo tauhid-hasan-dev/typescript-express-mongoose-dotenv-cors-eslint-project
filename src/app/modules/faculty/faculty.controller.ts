8e95529ee7934aee4626dc9cb57cde3994371d20
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2023-12-24 20:14:54 +0300

    auth gurad is checking sucessfully

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -4,8 +4,9 @@ import sendResponse from '../../utils/sendResponse';
 import { FacultyServices } from './faculty.service';
 
 const getSingleFaculty = catchAsync(async (req, res) => {
+  console.log(req.user);
   const { id } = req.params;
-  const result = await FacultyServices.getSingleFacultyFromDB( id );
+  const result = await FacultyServices.getSingleFacultyFromDB(id);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -29,7 +30,7 @@ const getAllFaculties = catchAsync(async (req, res) => {
 const updateFaculty = catchAsync(async (req, res) => {
   const { id } = req.params;
   const { faculty } = req.body;
-  const result = await FacultyServices.updateFacultyIntoDB( id , faculty);
+  const result = await FacultyServices.updateFacultyIntoDB(id, faculty);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -40,7 +41,7 @@ const updateFaculty = catchAsync(async (req, res) => {
 });
 
 const deleteFaculty = catchAsync(async (req, res) => {
-  const {  id } = req.params;
+  const { id } = req.params;
   const result = await FacultyServices.deleteFacultyFromDB(id);
 
   sendResponse(res, {
